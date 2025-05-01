70ae3e4bdfac7495830bf8f78474819a410de3b1
ipan1434 <flaelva6@gmail.com>
2025-05-01 21:03:45 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -5,7 +5,7 @@ load_dotenv(".env")
 
 MAX_BOT = int(os.getenv("MAX_BOT", "30"))
 
-DEVS = list(map(int, os.getenv("DEVS", "8099519433").split()))
+DEVS = list(map(int, os.getenv("DEVS", "6247987958").split()))
 
 API_ID = int(os.getenv("API_ID", "17823229"))
 
@@ -13,7 +13,7 @@ API_HASH = os.getenv("API_HASH", "94f4b70d2c78475d60b520847980e04d")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7436622533:AAFs-rL_5b4VHTtZJZehNnc9mrqXghwmgp4")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "8099519433"))
+OWNER_ID = int(os.getenv("OWNER_ID", "6247987958"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002692266809").split()))
 
